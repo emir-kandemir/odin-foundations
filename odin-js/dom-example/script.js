a4c606a7574e7ae8af1c8082f4d73fb43a4816be
emir-kandemir <227136149+emir-kandemir@users.noreply.github.com>
2026-05-15 12:29:08 +0300

    Add eventListener with fn

--- odin-js/dom-example/script.js
+++ odin-js/dom-example/script.js
@@ -1,2 +1,23 @@
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert('Hello World');
+
+/*const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert('Hello World');
+});*/
+
+// Method 1
+const alertFunction = () => alert('YAY! YOU DID IT!');
+
+// METHODS 2 & 3
 const btn = document.querySelector('#btn');
-btn.onclick = () => alert('Hello World');
+
+// Method 2
+btn.onclick = alertFunction;
+
+// Method 3
+btn.addEventListener('click', alertFunction);
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
